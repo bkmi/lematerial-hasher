353132d82b2c559c7af55496f9f8272127407954
vict0rsch <victor.schmidt@entalpic.ai>
2024-11-26 18:15:32 +0100

    refactor: ♻️ update hashers construction & docs

--- src/material_hasher/hashers/__init__.py
+++ src/material_hasher/hashers/__init__.py
@@ -1,4 +1,13 @@
 from material_hasher.hashers.lehash import LeHasher
 
-hashers = [LeHasher]
-hashers = {c.__name__.lower(): c for c in hashers}
+hashers = {
+    c.__name__.lower(): c
+    for c in [
+        LeHasher,
+    ]
+}
+"""
+A dictionary of available hashers.
+
+The keys are the names of the hashers (in lowercase) and the values are the corresponding hasher classes.
+"""
