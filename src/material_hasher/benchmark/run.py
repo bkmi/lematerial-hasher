902a330e485774392225bd9ecaae82b77cfde086
martinsiron <martin.siron@entalpic.ai>
2024-11-28 11:37:51 +0100

    fix to run to incorporate new tests cases and hasher funcs

--- src/material_hasher/benchmark/run.py
+++ src/material_hasher/benchmark/run.py
@@ -1,7 +1,18 @@
 from time import time
 from typing import Callable, Iterable, Optional
 
-from material_hasher.benchmark.test_cases import get_test_cases, get_tests
+from time import time
+from typing import Callable, Iterable, Optional
+
+from material_hasher.benchmark.test_cases import get_test_data, make_test_cases
+from material_hasher.hasher.entalpic import EntalpicMaterialsHasher
+from material_hasher.hasher.example import SimpleCompositionHasher
+
+from pymatgen.core import Structure
+
+HASHERS = {"Entalpic":
+           EntalpicMaterialsHasher,
+           "SimpleComposition":SimpleCompositionHasher}
 
 
 def load_structures():
@@ -11,58 +22,10 @@ def load_structures():
 
 
 def benchmark_hasher(
-    hasher: Callable,
-    test_cases: Optional[Iterable[str]] = None,
-    ignore_test_cases: Optional[Iterable[str]] = None,
-) -> float:
-    """Measure the performance of a hasher function based on test cases listed
-      in the :module:`material_hasher.benchmark.test_cases` module.
-
-    Parameters
-    ----------
-    hasher : Callable
-    Benchmark hashers.
-
-    This module provides a command-line interface to benchmark hashers. It can be run as a script or imported as a module.
-
-    Examples
-    --------
-
-    Run the benchmark for all hashers:
-
-    .. code-block:: bash
-
-        $ python -m material_hasher.benchmark.run --hasher all
-
-    Run the benchmark for a specific hasher:
-
-    .. code-block:: bash
-
-        $ python -m material_hasher.benchmark.run --hasher lehash
-
-    Use the hashers from the :mod:`material_hasher.hashers` package in your own code:
-
-    .. code-block:: python
-
-        from material_hasher.hashers import LeHasher
-
-        hasher = LeHasher()
-        ...
-
-    """
-
-
-from time import time
-from typing import Callable, Iterable, Optional
-
-from material_hasher.benchmark.test_cases import get_test_data, make_test_cases
-from material_hasher.hashers import hashers
-
-
-def benchmark_hasher(
     hasher_func: Callable,
     test_cases: Optional[Iterable[str]] = None,
     ignore_test_cases: Optional[Iterable[str]] = None,
+    structure_data: Optional[Iterable[Structure]] = None,
 ) -> dict[str, float]:
     """Measure the performance of a hasher function based on test cases listed in the :mod:`material_hasher.benchmark.test_cases` module.
 
@@ -88,14 +51,16 @@ def benchmark_hasher(
 
     test_cases = make_test_cases(test_cases, ignore_test_cases)
 
+    test_data = structure_data or load_structures()
+
     times = {"total": 0.0}
     for test_case in test_cases:
-        start_time = time()
-        test_data = get_test_data(test_case)
-        hasher_func(test_data)
-        end_time = time()
-        times[test_case] = end_time - start_time
-        times["total"] += times[test_case]
+        for structure in test_data:
+            start_time = time()
+            hasher_func(structure)
+            end_time = time()
+            times[test_case] = end_time - start_time
+            times["total"] += times[test_case]
 
     return times
 
@@ -117,7 +82,7 @@ def main():
     parser = ArgumentParser(description="Benchmark hashers.")
     parser.add_argument(
         "--hasher",
-        choices=list(hashers.keys()) + ["all"],
+        choices=list(HASHERS.keys()) + ["all"],
         help="The name of the hasher to benchmark.",
     )
     parser.add_argument(
@@ -132,7 +97,7 @@ def main():
     )
     args = parser.parse_args()
 
-    for hasher_name, hasher_class in hashers.items():
+    for hasher_name, hasher_class in HASHERS.items():
         if args.hasher != "all" and hasher_name != args.hasher:
             continue
 
