70f74adccf5db2cf07c18e99814ac1f2bfcb3ec0
martinsiron <martin.siron@entalpic.ai>
2024-12-06 09:54:57 +0100

    docstring for bonded structure graph

--- src/material_hasher/hasher/utils/graph_structure.py
+++ src/material_hasher/hasher/utils/graph_structure.py
@@ -1,13 +1,27 @@
 from pymatgen.analysis.graphs import StructureGraph
 from pymatgen.analysis.local_env import EconNN, NearNeighbors
 from pymatgen.core import Structure
+from networkx import Graph
 
 
 def get_structure_graph(
     structure: Structure,
     bonding_kwargs: dict = {},
     bonding_algorithm: NearNeighbors = EconNN,
-):
+) -> Graph:
+    """Method to build networkx graph object based on
+    bonding algorithm from Pymatgen Structure
+
+    Args:
+        structure (Structure): Pymatgen Structure object
+        bonding_kwargs (dict, optional): kwargs to pass to
+            NearNeighbor class. Defaults to {}.
+        bonding_algorithm (NearNeighbors, optional): NearNeighbor
+            class to build bonded structure. Defaults to EconNN.
+
+    Returns:
+        Graph: networkx Graph object
+    """
     structure_graph = StructureGraph.with_local_env_strategy(
         structure=structure,
         strategy=bonding_algorithm(**bonding_kwargs),
